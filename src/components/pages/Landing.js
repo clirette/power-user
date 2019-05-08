2008498dab320a05c90632505e8bf2abc41190e8
Chase Lirette <clirette@dxc.com>
2019-05-08 10:18:48 -0500

    Began work on the landing page hero

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Landing = () => (
-  <div>
-    
+  <div className="landing">
+    <section className="hero">
+      <div className="dark-overlay">
+        <div className="hero-text-box">
+          <h1 className="hero-text-box__heading">Power User</h1>
+          <p className="hero-text-box__lead">
+            Bring realtime analytics and cloud solutions to empower your
+            customers
+          </p>
+        </div>
+      </div>
+    </section>
   </div>
 );
 
